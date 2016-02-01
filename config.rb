937e2a02e4034749e48604fcbf4e356d748b2208
Bennie Mosher <benniemosher@gmail.com>
2016-01-31 23:53:17 -0700

    Basic styling of resume section

--- config.rb
+++ config.rb
@@ -1,57 +1,107 @@
-###
-# Page options, layouts, aliases and proxies
-###
+# Set the time zone
+Time.zone = 'America/Denver'
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
 
+# ------------------------------------------------------------------------
+# Formatters
+# ------------------------------------------------------------------------
+
+# Set up Haml and Markdown processors
 set :haml, ugly: true, format: :html5
 set :markdown, fenced_code_blocks: true, smartypants: true
 
-activate :directory_indexes
 
-Time.zone = 'America/Denver'
-
+# ------------------------------------------------------------------------
+# Blog Settings
+# ------------------------------------------------------------------------
 activate :blog do |blog|
 	blog.prefix = 'blog'
 	blog.paginate = true
 	blog.per_page = 5
 end
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# ------------------------------------------------------------------------
+# Helpers
+# ------------------------------------------------------------------------
 
-# General configuration
-
-# Reload the browser automatically whenever files change
-configure :development do
-	activate :livereload
+helpers do
+	# Helpers are defined in and can be added to `helpers/custom_helpers.rb`.
+	# In case you require helpers within `config.rb`, they can be added here.
 end
 
-###
-# Helpers
-###
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# ------------------------------------------------------------------------
+# Extensions
+# ------------------------------------------------------------------------
 
-# Build-specific configuration
+# Use LiveReload
+activate :livereload
+
+# Use autoprefixer
+activate :autoprefixer
+
+# Use Search Engine Sitemap
+set :url_root, data.config.site.root_url
+activate :search_engine_sitemap
+
+# User Bower to manage vendor scripts
+# activate :bower
+
+# Automatic image dimensions on image_tag helper (only works with local images)
+activate :automatic_image_sizes
+
+
+# ------------------------------------------------------------------------
+# Paths
+# ------------------------------------------------------------------------
+
+set :css_dir,       'stylesheets'
+set :fonts_dir,     'fonts'
+set :images_dir,    'images'
+set :js_dir,        'javascripts'
+
+# Pretty URLs - See https://middlemanapp.com/advanced/pretty_urls/
+activate :directory_indexes
+
+
+# ------------------------------------------------------------------------
+# Build configuration
+# ------------------------------------------------------------------------
+
 configure :build do
-	# Minify CSS on build
+	# Exclude any vendor components (bower or custom builds) in the build
+	ignore 'stylesheets/vendor/*'
+	ignore 'javascripts/vendor/*'
+
+	# Activate gzip compression
+	activate :gzip
+
+	# Minify CSS
 	activate :minify_css
 
-	# Minify Javascript on build
+	# Minify Javascript
 	activate :minify_javascript
+
+	# Minify HTML
+	activate :minify_html, {
+		remove_http_protocol: false,
+		remove_https_protocol: false,
+		remove_input_attributes: false,
+		remove_quotes: false
+	}
+
+	# Compress images (default)
+	require "middleman-smusher"
+	activate :smusher
+
+	# Compress ALL images (advanced)
+	# Before activating the below, follow setup instructions on https://github.com/toy/image_optim
+	# activate :imageoptim do |options|
+	#   options.pngout = false # set to true when pngout is also installed
+	# end
+
+	# Uniquely-named assets (cache buster)
+	# Exception: svg & png in images folder because they need to be interchangeable by JS
+	activate :asset_hash, ignore: [/images\/(.*\.png|.*\.svg)/]
 end
